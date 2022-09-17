4b463d704edc3cebcc683800f3dfa25672a7e9ce
jorgequevedomeli <jorge.quevedo@mercadolibre.com>
2022-09-17 17:29:49 -0300

    cambios

--- src/api/internal/controllers/filesController/controller.go
+++ src/api/internal/controllers/filesController/controller.go
@@ -1,11 +1,20 @@
 package filescontroller
 
 import (
-	"github.com/gin-gonic/gin"
 	"Hamming-Huffman-API/src/api/internal/services/filesService"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func ReadFile(c *gin.Context){
 	ctx := c.Request.Context()
-	filesservice.ReadFile(ctx)
+
+	fileString, err := filesservice.ReadFile(ctx)
+
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, fileString)
+	}
+
+	c.JSON(http.StatusOK, fileString)
 }
--- src/api/internal/services/filesService/service.go
+++ src/api/internal/services/filesService/service.go
@@ -2,9 +2,21 @@ package filesservice
 
 import (
 	"context"
+	"fmt"
 	"io/ioutil"
 )
 
-func ReadFile(ctx context.Context){
-	file, err := ioutil.ReadFile("../")
+func ReadFile(ctx context.Context) (string, error) {
+	fileString := ""
+
+	file, err := ioutil.ReadFile("./ejemplo.txt")
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return fileString, err
+	}
+
+	fileString = string(file)
+
+	return fileString, nil
 }
