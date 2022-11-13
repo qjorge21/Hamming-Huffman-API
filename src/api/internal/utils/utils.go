db271aa1e48a946d96e1ace29bc432009d06d83a
Jorge Quevedo <qjorge21@yahoo.com>
2022-11-13 19:44:18 -0300

    Logica para hamming helper 1024

--- src/api/internal/utils/utils.go
+++ src/api/internal/utils/utils.go
@@ -4,17 +4,6 @@ import (
 	"math"
 )
 
-// 2^p >= p + i + 1
-func CalculateParityBitsNeeded(i int) int {
-	for p := 0; p < i; p += 1 {
-		if int(math.Pow(2, float64(p))) < p+i+1 {
-			return p
-		}
-	}
-
-	return 0
-}
-
 func InvertirOrdenArreglo(arr []bool) []bool {
 	result := make([]bool, len(arr))
 
